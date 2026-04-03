fb7f8ca56f0bda435284cb272054e9d80dd5059a
aliamerj <aliamer19ali@gmail.com>
2026-04-03 09:22:26 +0300

    new blogs and update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://dcup.dev/docs',
+      url: 'https://openfeed.ink/docs',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://dcup.dev',
+      url: 'https://openfeed.ink/blog',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://openfeed.ink',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
